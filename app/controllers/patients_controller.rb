3e629f81080c8e2393a1b85ab1a007954cea1750
YiJieChuah <s3847905@student.rmit.edu.au>
2021-08-21 15:18:31 +1000

    Implemented relationship between users and patients in frontend.

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,9 +1,12 @@
 class PatientsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :correct_user, only: %i[ show edit update destroy ]
   before_action :set_patient, only: %i[ show edit update destroy ]
+
 
   # GET /patients or /patients.json
   def index
-    @patients = Patient.all
+    @patients = Patient.select {|p| p.user == current_user}
   end
 
   # GET /patients/1 or /patients/1.json
@@ -12,7 +15,8 @@ class PatientsController < ApplicationController
 
   # GET /patients/new
   def new
-    @patient = Patient.new
+    # @patient = Patient.new
+    @patient = current_user.patients.new
   end
 
   # GET /patients/1/edit
@@ -21,7 +25,7 @@ class PatientsController < ApplicationController
 
   # POST /patients or /patients.json
   def create
-    @patient = Patient.new(patient_params)
+    @patient = current_user.patients.new(patient_params)
 
     respond_to do |format|
       if @patient.save
@@ -56,6 +60,11 @@ class PatientsController < ApplicationController
     end
   end
 
+  def correct_user
+    @patient = current_user.patients.find_by(id: params[:id])
+    redirect_to root_path, notice: "Don't peek on other people's data!" if @patient.nil?
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_patient
@@ -64,6 +73,6 @@ class PatientsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def patient_params
-      params.require(:patient).permit(:name, :age, :ward_number, :notes)
+      params.require(:patient).permit(:name, :age, :ward_number, :notes, :user_id)
     end
 end
