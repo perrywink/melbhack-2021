1218d6e977083b0bf140a87469834ed3f29aad4f
YiJieChuah <s3847905@student.rmit.edu.au>
2021-08-21 20:18:59 +1000

    Update on seeds.rb to include more sample data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,16 +10,23 @@ user2.password = 'valid_password'
 user2.password_confirmation = 'valid_password'
 user2.save!
 
-p1 = user.patients.create(name:"Dan Melcher", age:20, ward_number:"2A")
-p2 = user.patients.create(name:"Vicky Koo", age:60, ward_number:"2B")
-p3 = user2.patients.create(name:"Owen Wilson", age:50, ward_number:"3A")
-p4 = user2.patients.create(name:"Rick May", age:53, ward_number:"3B")
+admin = User.new
+admin.email = 'admin@example.com'
+user2.password = 'valid_password'
+user2.password_confirmation = 'valid_password'
+user2.admin = true;
+user2.save!
 
-p1.logs.create(time_admr:"2021-07-20 06:04:00.000000000 +0000")
-p2.logs.create(time_admr:"2021-08-19 04:04:00.000000000 +0000")
+p1 = user.patients.create(name:"Dan Melcher", age:20, ward_number:"2A",notes:"Sample Notes")
+p2 = user.patients.create(name:"Vicky Koo", age:60, ward_number:"2B",notes:"Decrease dosage overtime.")
+p3 = user2.patients.create(name:"Owen Wilson", age:50, ward_number:"3A")
+p4 = user2.patients.create(name:"Rick May", age:53, ward_number:"3B", notes:"Increase dosage overtime.")
+
+log1 = p1.logs.create(time_admr:"2021-07-20 06:04:00.000000000 +0000")
+log2 = p2.logs.create(time_admr:"2021-08-19 04:04:00.000000000 +0000")
 p2.logs.create(time_admr:"2021-08-21 05:04:00.000000000 +0000")
 p2.logs.create(time_admr:"2021-08-23 02:04:00.000000000 +0000")
-p3.logs.create(time_admr:"2021-06-14 03:11:00.000000000 +0000")
+log3 = p3.logs.create(time_admr:"2021-06-14 03:11:00.000000000 +0000")
 p3.logs.create(time_admr:"2021-06-17 06:23:00.000000000 +0000")
 p3.logs.create(time_admr:"2021-06-20 10:33:00.000000000 +0000")
 p4.logs.create(time_admr:"2021-07-20 04:04:00.000000000 +0000")
@@ -29,5 +36,8 @@ Medicine.create(generic_name:"Acetaminophen", brand_name:"Actamin")
 Medicine.create(generic_name:"Viagra", brand_name:"Revatio")
 Medicine.create(generic_name:"Fentanyl", brand_name:"Fentora")    
 
+log1.doses.create(amount:250, medicine_id:1)
+log2.doses.create(amount:150, medicine_id:4)
+log3.doses.create(amount:200, medicine_id:3)
 
 
